acd4dcad061a89905731ec585b74a1d30ed19325
bayarbtr <bayarbtr@yahoo.com>
2021-02-19 09:08:44 +0800

    lastasfas dflasdf

--- app.js
+++ app.js
@@ -1,24 +1,68 @@
-var activePlayer = 1;
-var score = [0,0];
-var roundScore;
-var dice;
-var diceDom = document.querySelector('.dice')
+var activePlayer,score,roundScore,dice,diceNumber, isGameOver; 
+var diceDom = document.querySelector('.dice');
 
-var diceNumber = 0; //Math.floor(Math.random()*6)+1;
-//console.log(diceNumber);
+initGame();
 
-document.getElementById('score-0').textContent = diceNumber;
-document.getElementById('score-1').textContent = diceNumber;
-document.getElementById('current-0').textContent = diceNumber;
-document.getElementById('current-1').textContent = diceNumber;
-diceDom.style.display = 'none';
-
-document.querySelector(".btn-roll").addEventListener("click",shooShid);
-
-function shooShid(){
-    diceNumber = Math.floor(Math.random()*6)+1;
-    document.getElementById('current-0').textContent = diceNumber;
-    alert("Шоо буулаа: " + diceNumber);
-    diceDom.style.display = 'block';
-    diceDom.src = "dice-"+diceNumber+".png";
+function initGame(){
+    document.querySelector(".player-1-panel").classList.remove("winner");
+    document.querySelector(".player-0-panel").classList.remove("winner");
+    activePlayer = 0;
+    isGameOver = false;
+    score = [0,0];
+    roundScore=0;
+    dice=0;
+    diceNumber = 0; 
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+    diceDom.style.display = 'none';
 }
+
+
+document.querySelector(".btn-roll").addEventListener("click", function (){
+    if (isGameOver === false){
+    diceNumber = Math.floor(Math.random()*6)+1;
+    diceDom.src = "dice-"+diceNumber+".png";
+    diceDom.style.display = 'block';
+   
+    if (diceNumber !== 1) {
+        roundScore=roundScore+diceNumber;
+        document.getElementById('current-'+activePlayer).textContent = roundScore;
+        
+    }
+    else         speechtoNextPlayer();
+}
+}
+);
+
+document.querySelector(".btn-hold").addEventListener("click",function(){
+    if (isGameOver===false){
+    score[activePlayer]=score[activePlayer]+roundScore;
+    if (score[activePlayer]<=100)
+    {   isGameOver = true;
+        document.getElementById("name-"+activePlayer).textContent = "WINNER!!!";
+        document.querySelector(".player-" + activePlayer+ "-panel").classList.add("winner");
+        document.querySelector(".player-" + activePlayer+ "-panel").classList.toggle("active");
+            }
+    document.getElementById('score-'+activePlayer).textContent = score[activePlayer];
+    speechtoNextPlayer();
+        }
+});
+
+function speechtoNextPlayer(){
+    roundScore = 0;
+    document.getElementById('current-'+activePlayer).textContent = 0;
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    diceDom.style.display = 'none'; 
+}
+
+
+
+document.querySelector(".btn-new").addEventListener("click",function(){
+    if (isGameOver===false) alert("Тоглоом дуусаагүй байна!!!.")
+        else initGame();
+
+});
---
+++ asd.js
@@ -0,0 +1,84 @@
+
+var activePlayer;
+
+var scores;
+
+var roundScore;
+
+var diceDom = document.querySelector(".dice");
+
+initGame();
+
+function initGame() {
+  activePlayer = 0;
+
+  scores = [0, 0];
+
+  roundScore = 0;
+
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+
+  document.querySelector(".player-0-panel").classList.add("active");
+
+  diceDom.style.display = "none";
+}
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  diceDom.style.display = "block";
+
+  diceDom.src = "dice-" + diceNumber + ".png";
+
+  if (diceNumber !== 1) {
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    switchToNextPlayer();
+  }
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+  scores[activePlayer] = scores[activePlayer] + roundScore;
+
+  document.getElementById("score-" + activePlayer).textContent =
+    scores[activePlayer];
+
+  if (scores[activePlayer] >= 10) {
+    document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
+  } else {
+    switchToNextPlayer();
+  }
+});
+
+function switchToNextPlayer() {
+  roundScore = 0;
+  document.getElementById("current-" + activePlayer).textContent = 0;
+
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  diceDom.style.display = "none";
+}
+
+document.querySelector(".btn-new").addEventListener("click", initGame);
